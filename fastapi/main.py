9f02dad3938d2db776fa0b5969784085ebc82d6a
ArthurRufi <temparthur87@gmail.com>
2025-05-14 18:04:52 -0300

    commit de atualizacao de estudos sobre fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {'messagem aleatoria': "Minha vida"}
+
+@app.get("/low/{item}")
+async def room(item: int):
+    return {'messagem gravada': item}
