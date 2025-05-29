517c9168b00047a69f913fe46c1020700a5ca45a
ArthurRufi <temparthur87@gmail.com>
2025-05-29 18:49:07 -0300

    aprendendo sobre yeld

--- h3/h31/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Página carregada com sucesso.");
-// Exemplo: mostrar alerta ao clicar em um card
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', () => {
-    alert(`Você clicou em: ${card.textContent}`);
-  });
-});
